c4d60345d869ec0654b8c0f9907a634329c21ae3
jin kwong <davechunn@gmail.com>
2024-09-24 14:40:33 +0800

    Delete words.js

--- words.js
+++
@@ -1,4 +0,0 @@
-export const WORDS = ['frame','ethic','piece','place','zebra','value','check','prime','guess','print','pilot','final','drink','rival','crime','alarm','puppy','flame','night','alert'];
-
-
-
