4e918a64a96cea94e1db29ada3eeac3eed9f2850
jin kwong <davechunn@gmail.com>
2024-09-24 13:40:12 +0800

    Add files via upload

---
+++ words.js
@@ -0,0 +1,4 @@
+export const WORDS = ['frame','ethic','piece','place','zebra','value','check','prime','guess','print','pilot','final','drink','rival','crime','alarm','puppy','flame','night','alert'];
+
+
+
